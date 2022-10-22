761ed4f1df1f39a7b3a31e4739af53f729ed0f1d
LongDefeat <masonsams7@gmail.com>
2022-10-22 15:26:39 -0700

    Added code to display journal entries

--- frontend/src/journal/JournalList.js
+++ frontend/src/journal/JournalList.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect, useContext} from "react";
 import UserContext from "../auth/UserContext";
 import UserDatabaseApi from "../api/UserDatabaseApi";
+import MovieDatabaseApi from "../api/MovieDatabaseApi";
 import LoadingSpinner from "../common/LoadingSpinner";
 
 
 function JournalList(){
     const [journalEntries, setJournalEntries] = useState(null);
+    const [movieData, setMovieData] = useState(null);
     const currentUser = useContext(UserContext);
     console.log("Journal List Page: ", currentUser);
 
@@ -13,17 +15,43 @@ function JournalList(){
     if(currentUser) {
         useEffect(function getJournalEntries(){
             async function fetchJournalEntries(){
-                setJournalEntries(await UserDatabaseApi.getEntries(currentUser.id))};
+                setJournalEntries(
+                    await UserDatabaseApi.getEntries(currentUser.currentUser.id)
+                    );};
     
             fetchJournalEntries();
     
         }, [currentUser.id]);
     } 
-    // if (!journalEntries) return <LoadingSpinner />;
+    if (!journalEntries) return <LoadingSpinner />;
+    
+    const movieIds = journalEntries.map(e => {
+        return e.movie_id;
+    });
+
+    useEffect(function getArrayOfMovies(){
+        // sends array of movie ids
+        async function fetchMovieData(){
+            const unresolved = movieIds.map(async(id) => {
+                return await MovieDatabaseApi.getMovie(id);
+            });
+            const resolved = await Promise.all(unresolved);
+            console.log(resolved);
+
+            // setMovieData(
+            //     await 
+            // );
+        }
+    })
+
 
     console.log(journalEntries);
     return (
-        <h1>Journal Entries</h1>
+        <>
+        {journalEntries.map(e => {
+            return (<p>{e.comment}</p>)
+        })}
+        </>
     )
 }
 
